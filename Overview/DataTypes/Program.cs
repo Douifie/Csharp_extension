b5d3f5d9a81c28bc1998eacb4570e24ba226b599
Douifie <dakolodouifie@gmail.com>
2025-06-23 09:47:15 +0100

    space

--- Overview/DataTypes/Program.cs
+++ Overview/DataTypes/Program.cs
@@ -14,7 +14,7 @@ Console.WriteLine($"{a}, {b}, {tf}, {c}, {f}, {d}");
 // TODO: implicit type conversion
 Console.WriteLine($"{c + a}");
 Console.WriteLine($"{(char)(c + a)}");
-Console.WriteLine($"{f + a}"); 
+Console.WriteLine($"{f + a}");     
 Console.WriteLine($"{f + c}"); 
 // TODO: Create an instance of a struct (which is a value type)
 
